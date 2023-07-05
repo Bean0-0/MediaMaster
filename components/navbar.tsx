1d11a5cc5577a51aef9ec2b713e807c3bccd3c48
Bean <72772737+Bean0-0@users.noreply.github.com>
2023-07-05 01:01:10 +0000

    change nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -57,7 +57,7 @@ export const Navbar = () => {
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
 						<Logo />
-						<p className="font-bold text-inherit">ACME</p>
+						<p className="font-bold text-inherit">MediaMaster</p> 
 					</NextLink>
 				</NavbarBrand>
 				<ul className="hidden lg:flex gap-4 justify-start ml-2">
@@ -108,7 +108,7 @@ export const Navbar = () => {
 						startContent={<HeartFilledIcon className="text-danger" />}
 						variant="flat"
 					>
-						Sponsor
+						Sponsor Me
 					</Button>
 				</NavbarItem>
 			</NavbarContent>
